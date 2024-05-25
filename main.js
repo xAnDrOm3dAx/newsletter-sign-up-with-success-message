fd18feecea314748832ae0b48af6c81b83e082ff
xAnDrOm3dAx <123436349+xAnDrOm3dAx@users.noreply.github.com>
2024-05-25 13:18:20 +1000

    update script to display user email

--- main.js
+++ main.js
@@ -5,6 +5,7 @@ const errorMessage = document.querySelector("#error-message");
 const emailInput = document.querySelector("#user-email");
 const successMessage = document.querySelector(".success-message-wrapper");
 const articleContainer = document.querySelector(".article-container");
+const confirmationEmail = document.querySelector("#confirmation-email");
 
 form.addEventListener("submit", (e) => {
   e.preventDefault();
@@ -37,11 +38,13 @@ function clearError() {
 function DisplaySuccessMessage() {
   successMessage.classList.remove("hidden");
   articleContainer.classList.add("hidden");
+  confirmationEmail.textContent = emailInput.value + ".";
 }
 
 function removeSuccessMessage() {
   successMessage.classList.add("hidden");
   articleContainer.classList.remove("hidden");
   emailInput.value = "";
+  confirmationEmail.textContent = "ash@loremcompany.com.";
   clearError();
 }
