4a480c66b9dfa8f8997cfdf79013d6b9b10c5bde
xAnDrOm3dAx <123436349+xAnDrOm3dAx@users.noreply.github.com>
2024-05-28 20:43:46 +1000

    add dom content loaded

--- main.js
+++ main.js
@@ -1,50 +1,52 @@
-const form = document.querySelector("form");
-const submitBtn = document.querySelector("#submit-btn");
-const dismissBtn = document.querySelector("#dismiss-btn");
-const errorMessage = document.querySelector("#error-message");
-const emailInput = document.querySelector("#user-email");
-const successMessage = document.querySelector(".success-message-wrapper");
-const articleContainer = document.querySelector(".article-container");
-const confirmationEmail = document.querySelector("#confirmation-email");
+document.addEventListener("DOMContentLoaded", () => {
+  const form = document.querySelector("form");
+  const submitBtn = document.querySelector("#submit-btn");
+  const dismissBtn = document.querySelector("#dismiss-btn");
+  const errorMessage = document.querySelector("#error-message");
+  const emailInput = document.querySelector("#user-email");
+  const successMessage = document.querySelector(".success-message-wrapper");
+  const articleContainer = document.querySelector(".article-container");
+  const confirmationEmail = document.querySelector("#confirmation-email");
 
-form.addEventListener("submit", (e) => {
-  e.preventDefault();
-  validateEmail();
-});
+  form.addEventListener("submit", (e) => {
+    e.preventDefault();
+    validateEmail();
+  });
 
-dismissBtn.addEventListener("click", () => {
-  removeSuccessMessage();
-});
+  dismissBtn.addEventListener("click", () => {
+    removeSuccessMessage();
+  });
 
-function validateEmail() {
-  const userEmail = emailInput.value.trim();
-  const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+  function validateEmail() {
+    const userEmail = emailInput.value.trim();
+    const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
 
-  if (emailInput.value === "" || !emailRegex.test(userEmail)) {
-    displayError();
-  } else {
-    DisplaySuccessMessage();
+    if (emailInput.value === "" || !emailRegex.test(userEmail)) {
+      displayError();
+    } else {
+      DisplaySuccessMessage();
+    }
   }
-}
 
-function displayError() {
-  errorMessage.style.display = "block";
-}
+  function displayError() {
+    errorMessage.style.display = "block";
+  }
 
-function clearError() {
-  errorMessage.style.display = "none";
-}
+  function clearError() {
+    errorMessage.style.display = "none";
+  }
 
-function DisplaySuccessMessage() {
-  successMessage.classList.remove("hidden");
-  articleContainer.classList.add("hidden");
-  confirmationEmail.textContent = emailInput.value + ".";
-}
+  function DisplaySuccessMessage() {
+    successMessage.classList.remove("hidden");
+    articleContainer.classList.add("hidden");
+    confirmationEmail.textContent = emailInput.value + ".";
+  }
 
-function removeSuccessMessage() {
-  successMessage.classList.add("hidden");
-  articleContainer.classList.remove("hidden");
-  emailInput.value = "";
-  confirmationEmail.textContent = "ash@loremcompany.com.";
-  clearError();
-}
+  function removeSuccessMessage() {
+    successMessage.classList.add("hidden");
+    articleContainer.classList.remove("hidden");
+    emailInput.value = "";
+    confirmationEmail.textContent = "ash@loremcompany.com.";
+    clearError();
+  }
+});
