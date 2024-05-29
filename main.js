d3371473c6fb471508b0a7284ffb935f28cbbaac
xAnDrOm3dAx <123436349+xAnDrOm3dAx@users.noreply.github.com>
2024-05-29 11:06:02 +1000

    attempt to fix email validation issue

--- main.js
+++ main.js
@@ -5,7 +5,7 @@ const errorMessage = document.querySelector("#error-message");
 const emailInput = document.querySelector("#user-email");
 const successMessage = document.querySelector(".success-message-wrapper");
 const articleContainer = document.querySelector(".article-container");
-const confirmationEmail = document.querySelector("#confirmation-email");
+const emailConfirmation = document.querySelector("#email-confirmation");
 
 form.addEventListener("submit", (e) => {
   e.preventDefault();
@@ -38,13 +38,13 @@ function clearError() {
 function DisplaySuccessMessage() {
   successMessage.classList.remove("hidden");
   articleContainer.classList.add("hidden");
-  confirmationEmail.textContent = emailInput.value + ".";
+  emailConfirmation.textContent = emailInput.value + ".";
 }
 
 function removeSuccessMessage() {
   successMessage.classList.add("hidden");
   articleContainer.classList.remove("hidden");
   emailInput.value = "";
-  confirmationEmail.textContent = "ash@loremcompany.com.";
+  emailConfirmation.textContent = "ash@loremcompany.com.";
   clearError();
 }
